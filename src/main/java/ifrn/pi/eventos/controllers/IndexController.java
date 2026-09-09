910b04b34de4eeea1f98c4b06b33fe9155557633
Beatriz <beatriz.caeira@escolar.ifrn.edu.br>
2026-09-09 16:36:06 -0300

    Implementa a exibição dos detalhes do evento e ajusta navegação

--- src/main/java/ifrn/pi/eventos/controllers/IndexController.java
+++ src/main/java/ifrn/pi/eventos/controllers/IndexController.java
@@ -8,7 +8,7 @@ public class IndexController {
 	@RequestMapping("/")
 	public String index() {
 		System.out.println("Chamou o método index");
-		return"home";
+		return"redirect:/eventos";
 	}
 	
 }
