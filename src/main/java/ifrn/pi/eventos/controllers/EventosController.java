0b8d9719964b1fca5ee6b621c7492112873346d5
Beatriz <beatriz.caeira@escolar.ifrn.edu.br>
2026-09-22 11:31:10 -0300

    Cadastrando e listando os convidados

--- src/main/java/ifrn/pi/eventos/controllers/EventosController.java
+++ src/main/java/ifrn/pi/eventos/controllers/EventosController.java
@@ -1,41 +1,67 @@
 package ifrn.pi.eventos.controllers;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import ifrn.pi.eventos.models.Convidado;
 import ifrn.pi.eventos.models.Evento;
+import ifrn.pi.eventos.repositories.ConvidadoRepository;
 import ifrn.pi.eventos.repositories.EventoRepository;
 
 @Controller
 @RequestMapping("/eventos")
 public class EventosController {
 
-	@Autowired
-	private EventoRepository er;
+    @Autowired
+    private EventoRepository er;
+    
+    @Autowired
+    private ConvidadoRepository cr;
 
-	@RequestMapping("/form")
-	public String form() {
-		return "eventos/formEvento";
-	}
+    // Visualizar detalhes do evento e carregar os convidados
+    @GetMapping("/{id}")
+    public ModelAndView detalhar(@PathVariable Long id) {
+        ModelAndView md = new ModelAndView();
+        Optional<Evento> opt = er.findById(id);
 
-	@PostMapping("/eventos")
-	public String adicionar(Evento evento) {
-		System.out.println(evento);
-		er.save(evento);
-		return "eventos/evento-adicionado";
-	}
+        if (opt.isEmpty()) {
+            md.setViewName("redirect:/eventos");
+            return md;
+        }
 
-	@GetMapping
-	public ModelAndView listar() {
-		List<Evento> eventos = er.findAll();
-		ModelAndView mv = new ModelAndView("eventos/lista");
-		mv.addObject("eventos", eventos);
-		return mv;
-	}
+        md.setViewName("eventos/detalhes");
+        Evento evento = opt.get();
+        md.addObject("evento", evento);
+
+        List<Convidado> convidados = cr.findByEvento(evento);
+        md.addObject("convidados", convidados);
+
+        return md;
+    }
+
+    // Salvar convidado associado ao evento
+    @PostMapping("/{idEvento}")
+    public String salvarConvidado(@PathVariable Long idEvento, Convidado convidado) {
+        System.out.println("Id do evento: " + idEvento);
+        System.out.println(convidado);
+
+        Optional<Evento> opt = er.findById(idEvento);
+        if (opt.isEmpty()) {
+            return "redirect:/eventos";
+        }
+
+        Evento evento = opt.get();
+        convidado.setEvento(evento);
+        cr.save(convidado);
+
+        return "redirect:/eventos/" + idEvento;
+    }
 }
